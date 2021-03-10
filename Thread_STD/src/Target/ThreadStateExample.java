dc6425a81095225bfbe0675ce2f2544603b56b8c
ljglsg2 <68412346+ljglsg2@users.noreply.github.com>
2021-03-10 11:13:17 +0900

    타겟 스레드

---
+++ Thread_STD/src/Target/ThreadStateExample.java
@@ -0,0 +1,8 @@
+package Target;
+
+public class ThreadStateExample {
+	public static void main(String[] args) {
+		StatePrintThread statePrintThread = new StatePrintThread(new TargetThread());
+		statePrintThread.start();
+	}
+}
