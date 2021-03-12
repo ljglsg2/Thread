12b4b8431bb6646e5183565267adf4872bd3eeec
ljglsg2 <68412346+ljglsg2@users.noreply.github.com>
2021-03-12 17:37:48 +0900

    interrupt 1초후 출력 스레드 중지

---
+++ Thread_STD/src/Interrupt/InterruptExample.java
@@ -0,0 +1,15 @@
+package Interrupt;
+
+public class InterruptExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread = new PrintThread2();
+		thread.start();
+		
+		try { Thread.sleep(1000);}catch (InterruptedException e) {}
+		
+		thread.interrupt();
+	}
+
+}
