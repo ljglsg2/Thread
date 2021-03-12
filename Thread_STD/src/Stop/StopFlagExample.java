93cfefbe20b00403cc4da51d57f3a99c0c3e0ec0
ljglsg2 <68412346+ljglsg2@users.noreply.github.com>
2021-03-12 17:08:07 +0900

    Stop 플래그 [wile(!stop) Stop(true)]

---
+++ Thread_STD/src/Stop/StopFlagExample.java
@@ -0,0 +1,16 @@
+package Stop;
+
+public class StopFlagExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintThread1 printThread = new PrintThread1();
+		printThread.start();
+		
+		
+		try {Thread.sleep(1000);} catch (InterruptedException e) {}	
+		{
+			printThread.setStop(true);			//스레드를 종료시키기 위해 stop필트 true로 변경
+		}
+	}
+}
